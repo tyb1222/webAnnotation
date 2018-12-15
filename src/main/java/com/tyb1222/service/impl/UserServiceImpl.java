e6e8838dca2b3a689ff4abf022c650f4e8466f1c
taoyongbo <153026583@163.com>
2018-12-15 18:20:59 +0800

    refactor

--- src/main/java/com/tyb1222/service/impl/UserServiceImpl.java
+++ src/main/java/com/tyb1222/service/impl/UserServiceImpl.java
@@ -7,21 +7,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-@UserAnno(cacheKey = "class-cacheKey",cacheName ="USER_INFO", loggable = false)
+@UserAnno(cacheKey = "class-cacheKey",cacheName ="USER_INFO")
 public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserDao userDao;
-
-
 
     private final static String CACHE_KEY = "USER_INFO";
 
     @UserAnno(cacheKey = "#userId",cacheName =CACHE_KEY, loggable = true)
     public String getUserName(String userId) {
 
-        String userName = userDao.getUserName(userId);
+        return userDao.getUserName(userId);
 
-        return userName;
     }
 }
