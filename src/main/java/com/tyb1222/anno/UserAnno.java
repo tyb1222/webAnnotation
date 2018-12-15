cbebbc8b9344c0b2818d9060f76e8c89cebb1c66
tyb1222 <tyb1222@gmail.com>
2018-12-15 18:06:10 +0800

    Update UserAnno.java

--- src/main/java/com/tyb1222/anno/UserAnno.java
+++ src/main/java/com/tyb1222/anno/UserAnno.java
@@ -8,7 +8,7 @@ import java.lang.annotation.Target;
 @Target({ElementType.METHOD,ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface UserAnno {
-    String key();
+    String cacheKey();
 
     String cacheName();
 
